98f4800c571b7db3f242c5004ab51fd989be235f
Catrina Friday <cat.friday@gmail.com>
2020-06-22 17:59:41 -0400

    added methods to tag and dish

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,4 +3,42 @@ class Dish < ActiveRecord::Base
     has_many :dish_tags
     has_many :tags, through: :dish_tags
 
+    def self.names
+        Dish.all.map do |dish|
+            dish.name
+        end
+    end
+
+    def self.max_tags
+        Dish.all.max_by do |dish|
+            dish.tags.length 
+        end
+    end
+
+    def self.untagged
+        Dish.all.select do |dish|
+            dish.tags.length == 0
+        end
+    end
+
+    def self.average_tag_count
+       tag_count = Dish.all.map do |dish|
+            dish.tags.length
+        end
+        tag_count.sum / tag_count.length.to_f
+    end
+
+    def tag_count
+        self.tags.length 
+    end
+
+    def tag_names
+        self.tags.map  do |tag|
+            tag.name 
+        end
+    end
+
+    # def most_popular_tag
+
+    # end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,47 @@ class Tag < ActiveRecord::Base
     has_many :dish_tags
     has_many :dishes, through: :dish_tags
 
+    def self.most_common
+        Tag.all.max_by  do |tag|
+            tag.dishes.length
+        end
+    end
+
+    def self.least_common
+        Tag.all.min_by  do |tag|
+            tag.dishes.length
+        end   
+    end
+
+    def self.unused
+        Tag.all.select do |tag|
+            tag.dishes.length == 0
+        end    
+    end
+
+    def self.uncommon
+        Tag.all.select  do |tag|
+            tag.dishes.length < 5
+        end
+    end
+
+    def self.popular
+       pop_tag = Tag.all.sort_by do |tag| 
+            tag.dishes.length
+        end
+        pop_tag.reverse[0...5]
+    end
+
+    def restaurants
+       all_dishes = Tag.all.map  do |tag|
+            tag.dishes 
+        end.flatten
+        all_dishes.map  do |dish|
+            dish.restaurant
+        end.uniq 
+    end
+
+    def top_restaurant
+        self.restaurants 
+    end
 end
